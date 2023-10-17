710d20bbca439c7bcb4dc738291093fd0469731b
simoRancati <117301957+simoRancati@users.noreply.github.com>
2023-10-17 14:25:57 +0200

    Update Filtra_lunghezze.py

--- CreationDataset/Filtra_lunghezze.py
+++ CreationDataset/Filtra_lunghezze.py
@@ -1,4 +1,4 @@
-def filtra_sequenze(sequenze, lunghezza_minima, lunghezza_massima): # La lunghezza massima e minima la ecidiamo noi cioè io metto la mediana generale - 20 amminoacidi e + 20 amminoacidi
+def filtra_sequenze(sequenze, lunghezza_minima, lunghezza_massima): # We ecide the maximum and minimum length i.e. I put the general median - 20 amino acids and + 20 amino acids 
     indici = [i for i, seq in enumerate(sequenze) if lunghezza_minima <= len(seq) <= lunghezza_massima]
     sequenze_valide = [seq for i, seq in enumerate(sequenze) if lunghezza_minima <= len(seq) <= lunghezza_massima]
     return indici, sequenze_valide
