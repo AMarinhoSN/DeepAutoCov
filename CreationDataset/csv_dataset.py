ce3f99a8d9c7ee5f7badc3570bc61fbf65a579d2
simoRancati <117301957+simoRancati@users.noreply.github.com>
2023-10-17 14:23:21 +0200

    Update csv_dataset.py

--- CreationDataset/csv_dataset.py
+++ CreationDataset/csv_dataset.py
@@ -3,9 +3,9 @@ import csv
 
 
 def write_csv_dataset(array,l):
-    # Definizione dei nomi delle colonne come lista di stringhe
+    # Definition of column names as a list of strings.
     nomi_colonne = ['Virus.name','Not.Impo','format','Type','Accession.ID','Collection.date','Location','Additional.location.information','Sequence.length','Host','Patient.age','Gender','Clade','Pango.lineage','Pangolin.type','Variant','AA.Substitutions','Submission.date','Is.reference.','Is.complete.','Is.high.coverage.','Is.low.coverage.','N.Content']
-    # Apertura del file CSV in modalità scrittura e definizione del writer
+    # Opening the CSV file in write mode and defining the writer.
     with open('/blue/salemi/share/varcovid/filtered_metadatataset_010223_edit_020523_'+l+'.csv', "w", newline="") as csvfile:
         writer = csv.writer(csvfile, delimiter=",")
 
