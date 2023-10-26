ecc6ef55c5c3dccc32a584e6c6cfd7c66c93d9af
simoRancati <117301957+simoRancati@users.noreply.github.com>
2023-10-26 14:59:23 +0200

    Update Autoencoder_training.py

--- model/Autoencoder_training.py
+++ model/Autoencoder_training.py
@@ -1,3 +1,11 @@
+## INPUT
+# autoencoder: model defined in the script "model_dl.py"
+# train1 e train2: training set defined in the script "main_prediction_AE.py"
+# nb_epoch: number of epoch defined when you run the script "main_prediction_AE"
+# batch_size: batch_size defined when you run the script "main_prediction_AE"
+## OUTPUT
+# history: is the model trained  
+
 def Autoencoder_training(autoencoder, train1,train2,nb_epoch,batch_size):
     history = autoencoder.fit(train1, train2,
                                   epochs=nb_epoch,
