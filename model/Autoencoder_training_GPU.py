66920c6e8f79f18ad30ec7683c6d1b515e0d887b
simoRancati <117301957+simoRancati@users.noreply.github.com>
2023-10-26 14:52:06 +0200

    Update Autoencoder_training_GPU.py

--- model/Autoencoder_training_GPU.py
+++ model/Autoencoder_training_GPU.py
@@ -1,4 +1,8 @@
 import tensorflow as tf
+# autoencoder: model defined in the script "model_dl.py"
+# train1 e train2: training set defined in the script "main_prediction_AE.py"
+# nb_epoch: number of epoch defined when you run the script "main_prediction_AE"
+batch_size: batch_size defined when you run the script "main_prediction_AE"
 
 def autoencoder_training_GPU(autoencoder, train1, train2, nb_epoch, batch_size):
     # Assicurarsi che sia disponibile una GPU
