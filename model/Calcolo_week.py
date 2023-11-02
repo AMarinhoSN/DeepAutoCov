953775555430e8c7ac5ed28687c64b14b5228be2
simoRancati <117301957+simoRancati@users.noreply.github.com>
2023-11-02 19:57:09 +0100

    Update Calcolo_week.py

--- model/Calcolo_week.py
+++ model/Calcolo_week.py
@@ -1,18 +1,24 @@
 import numpy as np
 from collections import Counter
 
+# Calcolo_week : function that tells me how many weeks I need to predict 100 variants of the same type as anomalous
+# INPUT
+#    1)list that contains [['lineage1',predictions,week1],['lineage2',predictions,week1],['lineage2',predictions,week2]]. Where 'lineage' is the name of lineage, prediction is the number of predicte, prediction is the number of sequences defined as anomalous, week of simulation
+# OUTPUT
+#    2) list that contains the name of lineage and number of weeks to flag 100 sequences like anomalies 
+
 def Calcolo_week(a):
     a_np = np.array(a)
-    Varianti = a_np[:, 0]
+    Variants = a_np[:, 0]
     Prediction = a_np[:, 1]
     Prediction_int = [int(x) for x in Prediction]
     Week = a_np[:, 2]
     print(a_np[:, 1])
-    Variant = Counter(Varianti)
+    Variant = Counter(Variants)
     new_list = []
-    Riassunto_Finale = []
+    summary_Final = []
     for k in Variant.keys():
-        i_k = np.where(Varianti == k)[0]
+        i_k = np.where(Variants == k)[0]
         Variant_counter = Prediction[i_k]
         Variant_counter_int = [int(x) for x in Variant_counter]
         my_cum_sum_array = np.cumsum(Variant_counter_int)
@@ -23,9 +29,9 @@ def Calcolo_week(a):
         Interest_index = np.array(Index)
         Interest_index_min = Interest_index[:, 0]
         Index_to_week = i_k[Interest_index_min]
-        Settimana_obiettivo = int(Week[Index_to_week])
-        Indice_inizio = np.array(np.where(Varianti == k))
-        Settimana_inizio = int(Week[Indice_inizio[:, 0]])
-        riassunto = [k, Settimana_obiettivo - Settimana_inizio]
-        Riassunto_Finale.append(riassunto)
-    return np.array(Riassunto_Finale)
+        week_objective = int(Week[Index_to_week])
+        Index_start = np.array(np.where(Variants == k))
+        week_start = int(Week[Index_start[:, 0]])
+        summary = [k, week_objective - week_start]
+        summary_Final.append(summary)
+    return np.array(summary_Final)
