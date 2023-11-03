79b8ac5053cecc1ff17023b675c1e09ddbe5bf3e
simoRancati <117301957+simoRancati@users.noreply.github.com>
2023-11-03 12:28:56 +0100

    Update Filtra_lunghezze.py

--- Feature_Extraction/Filtra_lunghezze.py
+++ Feature_Extraction/Filtra_lunghezze.py
@@ -1,7 +1,13 @@
-def filtra_sequenze(sequenze, lunghezza_minima, lunghezza_massima): # We ecide the maximum and minimum length i.e. I put the general median - 20 amino acids and + 20 amino acids 
-    indici = [i for i, seq in enumerate(sequenze) if lunghezza_minima <= len(seq) <= lunghezza_massima]
-    sequenze_valide = [seq for i, seq in enumerate(sequenze) if lunghezza_minima <= len(seq) <= lunghezza_massima]
-    return indici, sequenze_valide
+
+# filtra_sequenze: function to filter the length of sequences 
+# INPUT:
+#    1) sequences: list
+#    2) length_minimum : minimum length acceptable 
+#    3) length_maximum : maximum length acceptable 
+def filtra_sequenze(sequences, length_minimum, length_maximum): # We ecide the maximum and minimum length i.e. I put the general median - 20 amino acids and + 20 amino acids 
+    index = [i for i, seq in enumerate(sequences) if length_minimum <= len(seq) <= length_maximum]
+    sequences_valid = [seq for i, seq in enumerate(sequences) if length_minimum <= len(seq) <= length_maximum]
+    return index, sequences_valid
 
 # sequenza=['ASTREFGIHILMONOPRST','ASTREFGIHILMONOPRST','A','BVG','ASTREFGIHILMONOPRST']
 # indici,sequenze_valide=filtra_sequenze(sequenza, 4, 22)
