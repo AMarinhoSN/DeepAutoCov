dc7400f745a07dff42f274b368e5a1aceb7f48ff
simoRancati <117301957+simoRancati@users.noreply.github.com>
2023-11-03 10:04:11 +0100

    Update get_lineage_class.py

--- model/get_lineage_class.py
+++ model/get_lineage_class.py
@@ -1,4 +1,9 @@
-
+# function that return a lineage name 
+# INPUT 
+# metadata: file csv that contains the information of sequences
+# id_list: list that contains the id of sequences
+# OUTPUT
+# variant_name_list: list that contains the name of lineages 
 def get_lineage_class(metadata, id_list):
     variant_name_list = []
     for id in id_list:
