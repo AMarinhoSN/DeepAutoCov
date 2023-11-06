d15c0dd831eb3f8e47803debee13184edee23e66
simoRancati <117301957+simoRancati@users.noreply.github.com>
2023-11-06 14:30:37 +0100

    Update csv_dataset.py

--- Feature_Extraction/csv_dataset.py
+++ Feature_Extraction/csv_dataset.py
@@ -6,7 +6,7 @@ def write_csv_dataset(array,l):
     # Definition of column names as a list of strings.
     nomi_colonne = ['Virus.name','Not.Impo','format','Type','Accession.ID','Collection.date','Location','Additional.location.information','Sequence.length','Host','Patient.age','Gender','Clade','Pango.lineage','Pangolin.type','Variant','AA.Substitutions','Submission.date','Is.reference.','Is.complete.','Is.high.coverage.','Is.low.coverage.','N.Content']
     # Opening the CSV file in write mode and defining the writer.
-    with open('/blue/salemi/share/varcovid/filtered_metadatataset_010223_edit_020523_'+l+'.csv', "w", newline="") as csvfile:
+    with open('/blue/salemi/share/varcovid/filtered_metadatataset_'+l+'.csv', "w", newline="") as csvfile:
         writer = csv.writer(csvfile, delimiter=",")
 
         # Writing header row with column names.
